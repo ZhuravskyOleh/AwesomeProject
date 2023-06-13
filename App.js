7f6ee3cb2f9131e0225424b8439a34092b535d90
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-06-13 14:52:49 +0300

    register page

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 import RegistrationScreen from './Screens/RegistrationScreen';
-import { SafeAreaView } from 'react-native';
 
 
 
@@ -18,16 +17,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
+    paddingTop: 263,
     flex: 1,
     justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
   },
 });
 
--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,22 +1,49 @@
 import React from "react";
-import { View, StyleSheet,Text, TextInput, Button } from "react-native";
+import {
+    View,
+    StyleSheet,
+    Text,
+    TextInput,
+    Image,
+    TouchableOpacity,
+    
+} from "react-native";
 
-export default function RegistrationScreen () {
-    return (
+const RegistrationScreen = () => {
+    return (        
         <View style={styles.wrap}>
-            <Text style={styles.title}>Реєстрація</Text>
+            <Text style={styles.title}>Реєстрація</Text>                  
             <TextInput style={styles.input} placeholder="Логін" />
             <TextInput style={styles.input} placeholder="Адреса електронної пошти" />
-            <TextInput style={styles.input} placeholder="Пароль" />
-            <Button title="Зареєстуватися" style={styles.button} />
-        </View>
+            <TextInput style={styles.input} placeholder="Пароль" />                
+            <TouchableOpacity style={styles.button} onPress={() => { }}>
+                <Text style={styles.buttonText}>Зареєстуватися</Text>
+            </TouchableOpacity>
+            <View style={styles.footerWrap}>
+                <TouchableOpacity>
+                    <Text style={styles.footerText}>Вже є акаунт?</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.footerText}>Увійти</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.photoWrapp}>
+                <TouchableOpacity style={styles.add}>
+                    <Image source={require('../assets/add.png')}/>
+                </TouchableOpacity>
+            </View>
+        </View>   
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     wrap: {
-        marginTop: 263,
+        position:'relative',
         paddingTop: 92,
+        paddingBottom: 45,
         height: "100%",
         backgroundColor: "#fff",
         borderTopLeftRadius: 25,
@@ -25,7 +52,7 @@ const styles = StyleSheet.create({
 
     title: {
         fontSize: 30,
-        fontWeight: 500,
+        fontWeight: 'bold',
         textAlign: 'center',
         marginBottom: 33,
     },
@@ -41,12 +68,57 @@ const styles = StyleSheet.create({
         borderColor: "#E8E8E8",
         borderRadius: 8,
         color: '#BDBDBD',
-        fontSize:16,
+        fontSize: 16,
     },
     button: {
-        marginTop:43,
-        // backgroundColor: "#FF6C00",
-        // width: 343,
-        // marginLeft:16,
+        marginTop: 43,
+        marginLeft: 16,
+        marginBottom: 32,
+        width: 343,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#FF6C00',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
+        lineHeight: 19,
+        fontWeight: 400,
+    },
+    footerWrap: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 5,
+        justifyContent: 'center',
+    },
+    footerText: {
+        fontSize: 16,
+        fontWeight: 400,
+        color: '#1B4371',
+        textDecorationLine: 'underline',
+    },
+    photoWrapp: {
+        position: 'absolute',
+        top: -60,
+        right:128,
+        width: 120,
+        height: 120,
+        backgroundColor: "#F6F6F6",
+        borderRadius:16,
+    },
+    add: {
+        position: 'absolute',
+        right: -12,
+        bottom:14,
+        width: 25,
+        height: 25,
+        borderRadius: '50%',
+        // borderWidth: 1,
+        // borderColor:'#FF6C00',
+    }
 });
+
+
+export default RegistrationScreen;
